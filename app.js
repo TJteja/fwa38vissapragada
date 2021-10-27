2930b8c65088cb9bd3c38de82990e25cc57e7d85
Sai Tejaswee Vissapragada <teja108116@gmail.com>
2021-10-27 17:44:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-router.get('/', function(req, res, next) {
+router.get('/mydata', function(req, res, next) {
   res.render('mydata', { title: 'Sai Tejaswee Vissapragada' });
 });
 
