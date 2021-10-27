111d89424d84dbbd2e4b966ee1c665e7cbe562a7
Sai Tejaswee Vissapragada <teja108116@gmail.com>
2021-10-27 17:58:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+router.get('/', function(req, res, next) {
+  res.render('mydata', { title: 'Sai Tejaswee Vissapragada' });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('mydata',mydataRouter)
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
