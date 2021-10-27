4a963ab7fef1c3622907627dc8014b13b52aff8e
Sai Tejaswee Vissapragada <teja108116@gmail.com>
2021-10-27 17:53:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-router.get('/', function(req, res, next) {
+router.get('/mydata', function(req, res, next) {
   res.render('mydata', { title: 'Express' });
 });
 var app = express();
