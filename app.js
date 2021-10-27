220d6e867eeb12a871b033de887d19be2c48f77d
Sai Tejaswee Vissapragada <teja108116@gmail.com>
2021-10-27 17:38:49 -0500

    ss6

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+router.get('/', function(req, res, next) {
+  res.render('mydata', { title: 'Express' });
+});
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+router.get('/', function(req, res, next) {
+  res.render('mydata', { title: 'Sai Tejaswee Vissapragada' });
+});
 
 app.use(logger('dev'));
 app.use(express.json());
